8ef1a6b6458119e1a1c7b1ae88b2e77ad0089d69
Giannis Tsagatakis <jtsagata@gmail.com>
2019-01-04 15:48:56 +0100

    Test fifo_buffer_3.vhd with RESET circuit

--- src/fifo_buffer_3.vhd
+++ src/fifo_buffer_3.vhd
@@ -1,30 +1,38 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.constants.ALL;
+
 
 entity fifo_buffer_3 is
     Port ( CLK : in  STD_LOGIC;
            RESET : in  STD_LOGIC;
            ENABLE : in  STD_LOGIC;
-           DIN : in  STD_LOGIC_VECTOR (7 downto 0);
-           DOUT : out  STD_LOGIC_VECTOR (7 downto 0);
-           DA : out  STD_LOGIC_VECTOR (7 downto 0);
-           DB : out  STD_LOGIC_VECTOR (7 downto 0);
-           DC : out  STD_LOGIC_VECTOR (7 downto 0);
+           DIN : in  STD_LOGIC_VECTOR (UBIT downto 0);
+           
+		   DOUT : out  STD_LOGIC_VECTOR (UBIT downto 0);
+           DA : out  STD_LOGIC_VECTOR (UBIT downto 0);
+           DB : out  STD_LOGIC_VECTOR (UBIT downto 0);
+           DC : out  STD_LOGIC_VECTOR (UBIT downto 0);
            READY : out  STD_LOGIC);
 end fifo_buffer_3;
 
 architecture Behavioral of fifo_buffer_3 is
-signal delay1, delay2, delay3, delay4 : STD_LOGIC_VECTOR (7 downto 0);
-signal buffer1, buffer2, buffer3, buffer4 : STD_LOGIC;
-signal empty_signal : STD_LOGIC := '1';
+signal delay1, delay2, delay3, delay4 : STD_LOGIC_VECTOR (UBIT downto 0);
+signal buffer1, buffer2, buffer3 : STD_LOGIC;
+signal buffer4, buffer5, buffer6 : STD_LOGIC;
+signal ready_signal : STD_LOGIC := '1';
 
 begin
 
+	--
+	-- Propagete numbers
+	-- 
     p1: process(CLK,RESET)
     begin
         if RESET = '1' then
             delay1 <= x"00";  delay2 <= x"00";
-            delay3 <= x"00";  delay4 <= x"00";      
+            delay3 <= x"00";  delay4 <= x"00"; 
         elsif rising_edge(CLK) then
             if ENABLE = '1' then
                 delay1 <= DIN;
@@ -40,19 +48,24 @@ begin
     DA <= delay1;
     DB <= delay2;
     DC <= delay3;
-    READY <= buffer3;	 
+    READY <= buffer4;	 
 
+	--
+	-- Get READY signal
+	-- 
     p2: process(CLK,RESET)
     begin
         if RESET = '1' then
             buffer1 <= '0';    buffer2 <= '0';
-            buffer3 <= '0';    buffer4 <= '0';          
+            buffer3 <= '0';    buffer4 <= '0'; 
         elsif rising_edge(CLK) then
             if ENABLE = '1' then
-                buffer1 <= empty_signal;
+				buffer1 <= '1';
                 buffer2 <= buffer1;
                 buffer3 <= buffer2;
-                -- buffer4 <= buffer3;
+                buffer4 <= buffer3;
+				--buffer5 <= buffer4;
+				--buffer6 <= buffer5;
             end if;
         end if;
     end process;	 
--- src/test/fifo_buffer_3_tb.vhd
+++ src/test/fifo_buffer_3_tb.vhd
@@ -2,6 +2,11 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE work.constants.ALL;
+
+-- require for std_logic etc.
+-- https://vhdlguide.readthedocs.io/en/latest/vhdl/testbench.html 
+USE std.textio.ALL;
+USE ieee.std_logic_textio.ALL; 
  
 ENTITY fifo_buffer_3_tb IS
 END fifo_buffer_3_tb;
@@ -16,10 +21,10 @@ ARCHITECTURE behavior OF fifo_buffer_3_tb IS
          RESET : IN  std_logic;
          ENABLE : IN  std_logic;
          DIN : IN  std_logic_vector(UBIT downto 0);
-         DOUT : OUT  std_logic_vector(7 downto 0);
-         DA : OUT  std_logic_vector(7 downto 0);
-         DB : OUT  std_logic_vector(7 downto 0);
-         DC : OUT  std_logic_vector(7 downto 0);
+         DOUT : OUT  std_logic_vector(UBIT downto 0);
+         DA : OUT  std_logic_vector(UBIT downto 0);
+         DB : OUT  std_logic_vector(UBIT downto 0);
+         DC : OUT  std_logic_vector(UBIT downto 0);
          READY : OUT  std_logic
         );
     END COMPONENT;
@@ -29,19 +34,35 @@ ARCHITECTURE behavior OF fifo_buffer_3_tb IS
    signal CLK : std_logic := '0';
    signal RESET : std_logic := '0';
    signal ENABLE : std_logic := '0';
-   signal DIN : std_logic_vector(7 downto 0) := (others => '0');
+   signal DIN : std_logic_vector(UBIT downto 0) := (others => '1');
 
  	--Outputs
-   signal DOUT : std_logic_vector(7 downto 0);
-   signal DA : std_logic_vector(7 downto 0);
-   signal DB : std_logic_vector(7 downto 0);
-   signal DC : std_logic_vector(7 downto 0);
+   signal DOUT : std_logic_vector(UBIT downto 0);
+   signal DA : std_logic_vector(UBIT downto 0);
+   signal DB : std_logic_vector(UBIT downto 0);
+   signal DC : std_logic_vector(UBIT downto 0);
    signal READY : std_logic;
 
    -- Clock period definitions
    constant CLK_period : time := 10 ns;
- 
+   signal CLOCK_EN: std_logic;
+
+    -- FILES
+    file debug_file : text;  -- text is keyword
+	
+	-- COUNT SIGNAL
+	signal COUNT : unsigned(UBIT downto 0) := (0=>'1', others => '0');
+
+--	-- RESET SIGNAL
+--	signal reset_signal : std_logic;
+	
 BEGIN
+	-- Process
+	-- CLK_process     (Handle clock)
+	-- start_circuit   (Handle RESET)
+	-- print_header    (Print the header once)
+	-- counter_process ( Do the counting)  ON (CLK,RESET)
+	-- debug_write     ( Print debug line) ON (CLK)
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: fifo_buffer_3 PORT MAP (
@@ -56,27 +77,113 @@ BEGIN
           READY => READY
         );
 
+   -- save data in file : path is relative to Modelsim-project directory
+   file_open(debug_file, "src/sims/fifo_3.txt", write_mode);
+
+--	-- Wire RESET
+--	RESET <= reset_signal;
+
    -- Clock process definitions
    CLK_process :process
    begin
 		CLK <= '0';
 		wait for CLK_period/2;
-		CLK <= '1';
+		CLK <= CLOCK_EN;
 		wait for CLK_period/2;
    end process;
  
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
+   -- start_circuit process
+   start_circuit: process
+   begin        
+        -- configure and enable
+        -- hold reset state for 100 ns.
+        RESET <= '1';
+		wait for CLK_period*10;      
+        RESET <= '0';
+		ENABLE <= '1';
+		CLOCK_EN <= '1';
+		
+		-- Test RESET circuit
+		wait for CLK_period*17;
+		RESET <= '1';
+		wait for CLK_period;
+		RESET <= '0';
 
-      wait for CLK_period*10;
-
-      -- insert stimulus here 
-
-      wait;
+		-- Test EARLY RESET circuit
+		wait for CLK_period*2;
+		RESET <= '1';
+		wait for CLK_period;
+		RESET <= '0';
+		
+        -- indefinitely suspend process
+		wait;  
    end process;
+   
+   
+   -- print header process
+   print_header: process
+   variable OLine : line;
+   begin        
+        -- Print file header
+        write(OLine, string'("DIN"));
+        write(OLine, ht);write(OLine, string'("|"));
+        write(OLine, string'("  DOUT"));
+        write(OLine, ht);write(OLine, string'("|"));
+        write(OLine, ht);write(OLine, string'("DC"));
+        write(OLine, ht);write(OLine, string'("DB"));
+        write(OLine, ht);write(OLine, string'("DA"));
+        write(OLine, ht);write(OLine, string'("|"));
+        write(OLine, ht);write(OLine, string'("RDY"));
+        writeline(debug_file, OLine);
+        
+        -- indefinitely suspend process
+		wait;  
+   end process;
+   
+   
+    --
+    -- Send sequential data to the buffer
+    --
+    counter_process: process(CLK,RESET)
+    variable OLine : line;
+	variable delay_print : std_logic := '0';
+	begin
+        -- There is a sensitivity list, no waits here
+        if(CLK'event and CLK='1' and ENABLE ='1') then
+            DIN <= std_logic_vector(COUNT);
+			COUNT <= COUNT + 1;
+			IF ( RESET ='1' ) THEN
+				COUNT <= (0=>'1', others => '0');
+				delay_print := '1';
+			END IF;
+			IF ( delay_print = '1' ) THEN
+				write(OLine, string'("[RESET event]"));
+				writeline(debug_file, OLine);
+				delay_print := '0';
+			END IF;
+        end if;
+    end process;
+	
+    --
+    -- Debug file write process
+    --
+    debug_write: process(CLK)
+        variable OLine : line;
+    begin
+        if(CLK'event and CLK='1' and ENABLE ='1') then
+            write(OLine, to_integer(unsigned(DIN)));
+            write(OLine, ht); write(OLine, string'("|"));
+            write(OLine, ht); write(OLine, to_integer(unsigned(DIN)));
+            write(OLine, ht); write(OLine, string'("|"));
+            write(OLine, ht); write(OLine, to_integer(unsigned(DC)));
+            write(OLine, ht); write(OLine, to_integer(unsigned(DB)));
+            write(OLine, ht); write(OLine, to_integer(unsigned(DA)));
+            write(OLine, ht); write(OLine, string'("|"));
+            write(OLine, ht); write(OLine, READY);
+            -- And now write the line to file
+            writeline(debug_file, OLine);
+        end if;
+    end process;
 
 END;
